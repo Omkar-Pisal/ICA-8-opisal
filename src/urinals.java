3db42959f6ae9a5ef1d85e0e15313e0dffaa433b
Omkar Pisal <opisal@asu.edu>
2022-10-27 11:40:51 -0700

    Added goodString Template

--- src/urinals.java
+++ src/urinals.java
@@ -4,6 +4,12 @@
  * @since   2022-1--27
  */
 public class urinals {
+
+    Boolean goodString( String str ) {  // checks to see if valid string
+        System.out.println ("Not yet implemented");
+        return true;
+    }
+
     public static void main(String[] args) {
 
     }
