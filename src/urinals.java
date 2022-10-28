bb2fc25947f02f704f73ce685afaf46ae7cf0fae
Omkar Pisal <opisal@asu.edu>
2022-10-27 22:28:49 -0700

    ====== Omkar Pisal == coutUrinals Method =======

--- src/urinals.java
+++ src/urinals.java
@@ -7,15 +7,45 @@ import java.util.Scanner;
  */
 public class urinals {
 
-    static boolean goodString(String str) {  // checks to see if valid string
-        if(str==null||str.length()==0||str.length()>=21||str.contains(" ")) return false;
+    public static boolean goodString(String str) {  // checks to see if valid string
+        if(str==null||str.length()==0||str.length()>=21||str.contains(" ")){
+            return false;
+        }
         for (int i=0; i<str.length()-1;i++){
-            if (str.charAt(i)==1 && str.charAt(i+1)==1) return false;
+            if (str.charAt(i)=='1' && str.charAt(i+1)=='1') {
+                return false;
+            }
         }
         return true;
     }
 
+    public static int countUrinals(String str)
+    {
+        StringBuilder stringBuilder = new StringBuilder(str);
+        int emptyUrinals = 0;
+        if(stringBuilder.charAt(0) == '0' && stringBuilder.charAt(1) == '0') {
+            stringBuilder.setCharAt(0, '1');
+            emptyUrinals = emptyUrinals + 1;
+        }
+        for(int i = 1; i< str.length() -1; i++)
+        {
+            if(stringBuilder.charAt(i-1) == '0' && stringBuilder.charAt(i) == '0' &&
+                    stringBuilder.charAt(i+1) == '0')
+            {
+                stringBuilder.setCharAt(i, '1');
+                emptyUrinals = emptyUrinals + 1;
+            }
+        }
+        if(stringBuilder.charAt(stringBuilder.length()-1) == '0' && stringBuilder.charAt(stringBuilder.length()-2) == '0')
+        {
+            stringBuilder.setCharAt(stringBuilder.length()-1, '1');
+            emptyUrinals = emptyUrinals +1;
+        }
+        return emptyUrinals;
+    }
+
     public static void main(String[] args) {
+        System.out.println(urinals.goodString("0101100"));
         Scanner scanner = new Scanner(System.in);
         System.out.println("1. Enter String manually\n2. Use File as input");
         int choice = scanner.nextInt();
--- test/urinalsTest.java
+++ test/urinalsTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -23,8 +24,8 @@ class urinalsTest {
     @Test
     @DisplayName("====== Omkar Pisal == Good String Method -> bad string test =======")
     void checkBadString() {
-        boolean check1 = urinals.goodString("0101100");
-        assertFalse(check1);
+        boolean check2 = urinals.goodString("0101100");
+        assertFalse(check2);
     }
 
     @Test
@@ -34,5 +35,11 @@ class urinalsTest {
         assertFalse(check1);
     }
 
+    @Test
+    @DisplayName("====== Omkar Pisal == coutUrinals Method =======")
+    void countUrinals() {
+        int emptyUrinals = urinals.countUrinals("10001");
+        assertEquals(1, emptyUrinals);
+    }
 
 }
